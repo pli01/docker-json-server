5a0b0d86e76db25cb83fa2446dc4c0d67e04a7cb
pli01 <pli01@users.noreply.github.com>
2020-01-21 15:33:23 +0100

    Load json-server.json default config

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ args="$@"
 
 args="$@ -p 80"
 
+file=/data/json-server.json
+if [ -f $file ]; then
+    echo "Found json-server.json config file, trying to open"
+    args="$args -c json-server.json"
+fi
+
 file=/data/db.json
 if [ -f $file ]; then
     echo "Found db.json, trying to open"
@@ -15,5 +21,6 @@ if [ -f $file ]; then
     echo "Found file.js seed file, trying to open"
     args="$args file.js"
 fi
+
 
 json-server $args
